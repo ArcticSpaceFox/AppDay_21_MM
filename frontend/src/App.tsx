bbc58f50946f687ca56d91f53a4b054918baf462
ArcticSpaceFox <Sm03leBr00t@protonmail.com>
2021-05-28 15:01:05 +0200

    :sparkles: Added Tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="max-w-5xl mx-auto">
+        <img src={logo} className="h-26 w-26" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
